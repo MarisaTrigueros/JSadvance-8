df1ede98e172846ce9f6d25e0ee18d36e06d153d
MarisaTrigueros <159051163+MarisaTrigueros@users.noreply.github.com>
2024-04-29 23:13:56 +0200

    Actualización 8.Closures.js

--- 8.Closures.js
+++ 8.Closures.js
@@ -1,8 +1,21 @@
+// Forma 1:
+
 function multiplyByTwo(number) {
     function inner() {
         return number * 2;
     }
     return inner;
+}
+
+console.log(multiplyByTwo(4)());
+
+// Forma 2
+function multiplyByTwo(numbers) {
+    let otherNumber=2;
+    function innere() {
+        return numbers * otherNumber;
+    }
+    return innere;
 }
 
 console.log(multiplyByTwo(4)());
